39d069e9118e15fb45639a7feb9ff32550b47077
DawitSisayy <dawitsisay42@gmail.com>
2022-10-28 12:24:50 +0300

    first commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'vo0z3kaxxh';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'dev-tcyieg68phbpmsds.us.auth0.com',
+  clientId: 'qRLyA4fH9n1EJEBrFUDJ7ccZSDMM3vQM',
+  callbackUrl: 'http://localhost:3000/callback'
+};
